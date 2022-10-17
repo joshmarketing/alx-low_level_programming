3831d2299a61605fb358a1dab7e375e88bee3c4c
joshmarketing <joshmarketingagency@gmail.com>
2022-10-17 16:14:00 -0700

    final tasks

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,17 +5,17 @@
  * Return: Always 0
  */
 int main(void)
-{
-		int n;
-
-		for (n = 48; n < 58; n++)
+{	
+	int n;
+	
+	for (n = 48; n < 58; n++)
+	{	
+		putchar(n);
+		if (n != 57)
 		{
-			putchar(n);
-			if (n != 57)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		putchar('\n');
-		return (0);
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
 }
